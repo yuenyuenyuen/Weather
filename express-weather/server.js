9f198bc74525a85c98e4912fa957b5269ae19ef0
yuenyuenyuen <126215542+yuenyuenyuen@users.noreply.github.com>
2023-09-13 19:40:17 +0800

    Update server.js

--- express-weather/server.js
+++ express-weather/server.js
@@ -5,7 +5,7 @@ const express = require('express');
 const fetch = (...args) => import('node-fetch').then(({default: fetch}) => fetch(...args));
 
 const app = express();
-const APIKEY = "";  //signup at api.openweathermap.org and obtain an API Key
+const APIKEY = "e85fc0b5ae3903b46212ecd0d17b12d6";  //signup at api.openweathermap.org and obtain an API Key
 
 var options = {
     host: 'api.openweathermap.org',
